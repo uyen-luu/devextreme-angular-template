1db65ed2f22f54c8a21caec2a581164a4339d765
Uyen Luu <it.luudinhuyen@gmail.com>
2021-05-30 11:06:06 +0700

    Update layout

--- src/app/modules/tasks/tasks.module.ts
+++ src/app/modules/tasks/tasks.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {DxDataGridModule} from 'devextreme-angular';
 import {AuthGuardService} from '../../shared/services';
 import {SharedModule} from '../../shared/shared.module';
 import {ThemeModule} from '../../theme/theme.module';
